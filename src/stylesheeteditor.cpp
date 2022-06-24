a6252687c2866eee903f5b7b405fd9caf1ebc02a
alirezah95 <alirezahassani95@gmail.com>
2022-06-24 19:42:44 +0430

    feat: add getter for line nums area width, add a slot to update it

--- src/stylesheeteditor.cpp
+++ src/stylesheeteditor.cpp
@@ -3,3 +3,17 @@
 StyleSheetEditor::StyleSheetEditor(QWidget* parent) : QTextEdit(parent) { }
 
 StyleSheetEditor::~StyleSheetEditor() { }
+
+int StyleSheetEditor::getLineNumberAreaWidth() const
+{
+    // Count the digits of the doucument's block count
+    int lineCount = qMax(1, document()->blockCount());
+    int digitCount = 0;
+    while (lineCount > 0) {
+        lineCount /= 10;
+        digitCount++;
+    }
+    return (fontMetrics().boundingRect('0').width() * digitCount + 16);
+}
+
+void StyleSheetEditor::updateLineNumbersAreaWidth() { }
--- src/stylesheeteditor.h
+++ src/stylesheeteditor.h
@@ -1,13 +1,15 @@
 #ifndef STYLESHEETEDITOR_H
 #define STYLESHEETEDITOR_H
 
-#include <QWidget>
+#include <QTextEdit>
+
+class QWidget;
 
 namespace Ui {
 class StyleSheetEditor;
 }
 
-class StyleSheetEditor : public QWidget
+class StyleSheetEditor : public QTextEdit
 {
     Q_OBJECT
 
@@ -15,8 +17,24 @@ public:
     explicit StyleSheetEditor(QWidget* parent = nullptr);
     ~StyleSheetEditor();
 
+    /*!
+     * \brief Returns the width required for the line numbers area so that the
+     * numbers are properly shown on it.
+     * \return An integer as the width
+     */
+    int getLineNumberAreaWidth() const;
+
+private slots:
+    /*!
+     * \brief Sets a margin to the text edit to make enough room for line
+     * numbers area widget.
+     */
+    void updateLineNumbersAreaWidth();
+
 private:
-    Ui::StyleSheetEditor* ui;
+    QWidget* mLineNumberArea; /*!< A \a\b QWidget used for drawing line numbers
+                               * onto
+                               */
 };
 
 #endif // STYLESHEETEDITOR_H
