91bbf8a9f2937045193772131ee4bc885f618752
alirezah95 <alirezahassani95@gmail.com>
2022-06-25 02:30:07 +0430

    feat: add vars for preview and style editor, add them to mainwindow

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,10 +1,28 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
+#include "stylesheeteditor.h"
+#include "widgetspreview.h"
+
+#include <QHBoxLayout>
+#include <QSplitter>
+
 MainWindow::MainWindow(QWidget* parent)
-    : QMainWindow(parent), ui(new Ui::MainWindow)
+    : QMainWindow(parent), ui(new Ui::MainWindow),
+      mStyleEditor(new StyleSheetEditor), mPreview(new WidgetsPreview)
 {
     ui->setupUi(this);
+
+    auto splitter = new QSplitter;
+    splitter->addWidget(mStyleEditor);
+    splitter->addWidget(mPreview);
+
+    auto hbox = new QHBoxLayout;
+    hbox->setContentsMargins(0, 0, 0, 0);
+    hbox->addWidget(splitter);
+
+    ui->centralwidget->setLayout(hbox);
+    return;
 }
 
 MainWindow::~MainWindow()
