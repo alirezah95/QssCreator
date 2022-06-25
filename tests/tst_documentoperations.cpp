cf657ec57e785eb9d13f56b5e2de91197df6ccc1
alirezah95 <alirezahassani95@gmail.com>
2022-06-26 00:00:53 +0430

    tests: initialize test for DocumentOperations class

---
+++ tests/tst_documentoperations.cpp
@@ -0,0 +1,18 @@
+#include <QCoreApplication>
+#include <QTest>
+
+class TestDocumentOperations : public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void testNewFile();
+    void testOpenFile();
+    void testSaveFile();
+};
+
+void TestDocumentOperations::testNewFile() { }
+
+void TestDocumentOperations::testOpenFile() { }
+
+void TestDocumentOperations::testSaveFile() { }
