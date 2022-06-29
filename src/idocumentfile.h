ccbfeb9420b6bfa6b521edbcdf67d009e5419a82
alirezah95 <alirezahassani95@gmail.com>
2022-06-29 12:40:18 +0430

    refactor: change ctor to receive a QObject parent

--- src/idocumentfile.h
+++ src/idocumentfile.h
@@ -12,8 +12,8 @@ class IDocumentFile : public QObject
 {
     Q_OBJECT
 public:
-    IDocumentFile() = default;
-    virtual ~IDocumentFile();
+    IDocumentFile(QObject* parent = nullptr) : QObject(parent) {};
+    virtual ~IDocumentFile() {};
 
     virtual bool exists() const = 0;
     virtual bool open(QIODevice::OpenMode mode) = 0;
