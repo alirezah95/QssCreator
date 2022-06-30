810bc1d55106106e5e40d2ef70c5f747eb5956b3
alirezah95 <alirezahassani95@gmail.com>
2022-06-30 22:11:33 +0430

    fix: create MainWindow's required dependencies before instancing it

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,18 @@
 #include "mainwindow.h"
+#include "qssdeditor.h"
+#include "qssdfileoperations.h"
 
 #include <QApplication>
 
 int main(int argc, char* argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
+
+    auto editor = new QssdEditor;
+    auto docOper = new QssdFileOperations;
+
+    MainWindow w(editor, docOper);
     w.show();
+
     return a.exec();
 }
