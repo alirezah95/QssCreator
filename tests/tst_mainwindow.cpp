4183915f21ce43bcfd232ad2dd8db203990034a8
alirezah95 <alirezahassani95@gmail.com>
2022-07-01 16:47:21 +0430

    tests: add a test for undo/redo action

--- tests/tst_mainwindow.cpp
+++ tests/tst_mainwindow.cpp
@@ -1,14 +1,68 @@
+#include <QAction>
 #include <QApplication>
+#include <QTest>
+#include <QToolBar>
+
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
 #include "mainwindow.h"
 
-TEST(TestMainWindow, testSignals) { }
+#include "tst_mockqssdeditor.h"
+#include "tst_mockqssdfileoperations.h"
+
+class TestMainWindow : public ::testing::Test
+{
+public:
+    void SetUp()
+    {
+        editorMock = new MockQssdEditor;
+        opersMock = new MockQssdFileOperations;
+
+        mainWin = new MainWindow(editorMock, opersMock);
+    }
+
+    void TearDown()
+    {
+        delete mainWin;
+        return;
+    }
+
+    MockQssdEditor* editorMock;
+    MockQssdFileOperations* opersMock;
+
+    MainWindow* mainWin;
+};
+
+TEST_F(TestMainWindow, testUndoRedo)
+{
+    const auto& actions
+        = mainWin->findChild<QToolBar*>("mainToolBar")->actions();
+
+    auto undoAct = std::find_if(actions.begin(), actions.end(),
+        [](QAction* curr) { return curr->objectName() == "actionUndo"; });
+
+    auto redoAct = std::find_if(actions.begin(), actions.end(),
+        [](QAction* curr) { return curr->objectName() == "actionRedo"; });
+
+    ASSERT_NE(undoAct, actions.end());
+    ASSERT_NE(redoAct, actions.end());
+
+    editorMock->insertPlainText("Text");
+
+    EXPECT_EQ((*undoAct)->isEnabled(), true);
+    EXPECT_EQ((*redoAct)->isEnabled(), false);
+
+    QTest::keyPress(editorMock, Qt::Key_Z, Qt::ControlModifier);
+
+    EXPECT_EQ((*undoAct)->isEnabled(), false);
+    EXPECT_EQ((*redoAct)->isEnabled(), true);
+}
 
 int main(int argc, char* argv[])
 {
-    testing::InitGoogleMock(&argc, argv);
+    QApplication app(argc, argv);
 
+    testing::InitGoogleMock(&argc, argv);
     return RUN_ALL_TESTS();
 }
