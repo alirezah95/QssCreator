cd6b7b3cec054dec45f14a3ef49d0c69dbe13d3b
alirezah95 <alirezahassani95@gmail.com>
2022-07-01 15:58:11 +0430

    tests: add a mocl for IQssdFileOperations
    Add it to TestMainWindow

---
+++ tests/tst_mockqssdfileoperations.h
@@ -0,0 +1,24 @@
+#ifndef TST_MOCKQSSDFILEOPERATIONS_H
+#define TST_MOCKQSSDFILEOPERATIONS_H
+
+#include "iqssdfileoperations.h"
+
+#include <gmock/gmock.h>
+
+class MockQssdFileOperations : public IQssdFileOperations
+{
+public:
+    explicit MockQssdFileOperations(QObject* parent = nullptr)
+        : IQssdFileOperations(parent) {};
+    virtual ~MockQssdFileOperations() { }
+
+    MOCK_METHOD(bool, newDocument, (QTextEdit * editor), (override));
+    MOCK_METHOD(bool, openDocument,
+        (QTextEdit * editor, IDocumentFile* docFile), (override));
+    MOCK_METHOD(bool, saveDocument,
+        (const QTextEdit* editor, IDocumentFile* outFile), (override));
+    MOCK_METHOD(bool, exportDocument,
+        (const QTextEdit* editor, IDocumentFile* outFile), (override));
+};
+
+#endif // TST_MOCKQSSDFILEOPERATIONS_H
