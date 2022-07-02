5c6973909545f15f5ad88d08f3a69a0f4d760891
alirezah95 <alirezahassani95@gmail.com>
2022-07-02 21:56:52 +0430

    feat: add a QTextEdit pointer, add two methods
    One method for setting the internal text editor pointer
    One method to set replace operation enabled or diabled

--- src/findreplacedialog.cpp
+++ src/findreplacedialog.cpp
@@ -7,7 +7,27 @@ FindReplaceDialog::FindReplaceDialog(QWidget* parent)
     ui->setupUi(this);
 }
 
+FindReplaceDialog::FindReplaceDialog(QTextEdit* txtEdit, QWidget* parent)
+    : QDialog(parent), mTextEdit(txtEdit)
+{
+}
+
 FindReplaceDialog::~FindReplaceDialog()
 {
     delete ui;
+}
+
+void FindReplaceDialog::setTextEdit(QTextEdit* txtEdit)
+{
+    mTextEdit = txtEdit;
+    return;
+}
+
+void FindReplaceDialog::setReplaceEnabled(bool repEnabled)
+{
+    ui->replaceBtn->setVisible(repEnabled);
+    ui->findReplaceBtn->setVisible(repEnabled);
+    ui->replaceLbl->setVisible(repEnabled);
+    ui->replaceLEdit->setVisible(repEnabled);
+    return;
 }
--- src/findreplacedialog.h
+++ src/findreplacedialog.h
@@ -7,16 +7,33 @@ namespace Ui {
 class FindReplaceDialog;
 }
 
+class QTextEdit;
+
 class FindReplaceDialog : public QDialog
 {
     Q_OBJECT
 
 public:
     explicit FindReplaceDialog(QWidget* parent = nullptr);
+    FindReplaceDialog(QTextEdit* txtEdit, QWidget* parent);
     ~FindReplaceDialog();
+
+    /*!
+     * \brief Sets the text editor for find/replace operations
+     * \param txtEdit
+     */
+    void setTextEdit(QTextEdit* txtEdit);
+
+    /*!
+     * \brief Set replace operations enabled or disabled
+     * \param repEnabled
+     */
+    void setReplaceEnabled(bool repEnabled);
 
 private:
     Ui::FindReplaceDialog* ui;
+
+    QTextEdit* mTextEdit = nullptr;
 };
 
 #endif // FINDREPLACEDIALOG_H
