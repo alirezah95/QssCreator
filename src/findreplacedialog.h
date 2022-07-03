50f50acae6e9af594c6197f7d0c00583809e9bb5
alirezah95 <alirezahassani95@gmail.com>
2022-07-03 21:05:11 +0430

    feat: add two QTextCharFormat for find and current occurence texts
    docs: add doxycomments

--- src/findreplacedialog.h
+++ src/findreplacedialog.h
@@ -2,6 +2,7 @@
 #define FINDREPLACEDIALOG_H
 
 #include <QDialog>
+#include <QTextCharFormat>
 
 namespace Ui {
 class FindReplaceDialog;
@@ -31,20 +32,48 @@ public:
     void setReplaceEnabled(bool repEnabled);
 
 private slots:
+    /*!
+     * \brief Slot to find all the occurences.
+     * \param text The text to search for occurences
+     */
     void findAllOccurences(const QString& text);
 
+    /*!
+     * \brief Finds the next occurence of the text in line edit and highlight it
+     */
     void onFindNextButtonPressed();
 
+    /*!
+     * \brief Finds the previout occurence of the text in line edit and
+     * highlight it
+     */
     void onFindPrevButtonPressed();
 
+    /*!
+     * \brief Replace the text under the cursor with the text in replace line
+     * edit
+     */
     void onReplaceButtonPressed();
 
+    /*!
+     * \brief Replace the text under the cursor with the text in replace line
+     * edit and calls the \ref onFindNextButtonPressed()
+     */
     void onFindReplaceButtonPressed();
 
 private:
     Ui::FindReplaceDialog* ui;
 
     QTextEdit* mTextEdit = nullptr;
+
+    QTextCharFormat mFindFormat; /*!< The format that is used for highlighting
+                                  * text occurences.
+                                  */
+
+    QTextCharFormat mHighlightFormat; /*!< The format that is used for text
+                                       * occurence that the cursor is currently
+                                       * on.
+                                       */
 };
 
 #endif // FINDREPLACEDIALOG_H
