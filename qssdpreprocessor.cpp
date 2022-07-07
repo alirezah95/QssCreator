59a0d221e9ee600b180121a8cdec5af4d54e3c6c
alirezah95 <alirezahassani95@gmail.com>
2022-07-07 12:34:53 +0430

    feat: initializes methods for preprocessing and getting processed doc

--- qssdpreprocessor.cpp
+++ qssdpreprocessor.cpp
@@ -1,7 +1,33 @@
 #include "qssdpreprocessor.h"
 
-QssdPreprocessor::QssdPreprocessor(QObject *parent)
-    : QObject{parent}
+#include "iqssdeditor.h"
+
+QssdPreprocessor::QssdPreprocessor(QObject* parent)
+    : QObject { parent }, mEditor(nullptr)
+{
+}
+
+QssdPreprocessor::QssdPreprocessor(IQssdEditor* editor, QObject* parent)
+    : QssdPreprocessor(parent)
+{
+    setQssdEditor(editor);
+    return;
+}
+
+QssdPreprocessor::~QssdPreprocessor() { }
+
+void QssdPreprocessor::setQssdEditor(IQssdEditor* editor)
+{
+    if (mEditor) {
+        connect(mEditor->document(), &QTextDocument::modificationChanged, this,
+            &QssdPreprocessor::preProcessDocument);
+    }
+    return;
+}
+
+QString QssdPreprocessor::getProcessedDocumentContent()
 {
 
 }
+
+void QssdPreprocessor::preProcessDocument(bool docIsModified) { }
--- qssdpreprocessor.h
+++ qssdpreprocessor.h
@@ -3,14 +3,41 @@
 
 #include <QObject>
 
+class IQssdEditor;
+
+/*!
+ * \brief The QssdPreprocessor class which is responsible for processing a
+ * document inside a \ref IQssdEditor and updating the variables in document
+ * with a \ref IQssdVariableModel and vice versa. And also it is responsible for
+ * replaced variables occurences in a document with the actual value of the
+ * variable
+ */
 class QssdPreprocessor : public QObject
 {
     Q_OBJECT
 public:
-    explicit QssdPreprocessor(QObject *parent = nullptr);
+    explicit QssdPreprocessor(QObject* parent = nullptr);
+    QssdPreprocessor(IQssdEditor* editor, QObject* parent = nullptr);
+    ~QssdPreprocessor();
 
-signals:
+    /*!
+     * \brief Sets the internal pointer to a \ref IQssdEditor
+     * \param editor A \ref IQssdEditor
+     */
+    void setQssdEditor(IQssdEditor* editor);
 
+    /*!
+     * \brief Processes the document, replaces all variables with theri values
+     * and returning a \a\b QString as the result
+     * \return \a\b QString as the result of processing
+     */
+    QString getProcessedDocumentContent();
+
+private slots:
+    void preProcessDocument(bool docIsModified);
+
+private:
+    IQssdEditor* mEditor;
 };
 
 #endif // QSSDPREPROCESSOR_H
