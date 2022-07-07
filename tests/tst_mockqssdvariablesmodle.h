7e6ebad8fab9be18237d45eaa9cb82e242470793
alirezah95 <alirezahassani95@gmail.com>
2022-07-07 17:44:42 +0430

    tests: rename abs list model mock, update it to mock IQssdVariablesModel

--- tests/tst_mockqssdvariablesmodle.h
+++ tests/tst_mockqssdvariablesmodle.h
@@ -1,9 +1,12 @@
-#include <QAbstractListModel>
+#ifndef TST_MOCKQSSDVARIABLESMODLE_H
+#define TST_MOCKQSSDVARIABLESMODLE_H
+
+#include "iqssdvariablesmodel.h"
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
-class MockListModel : public QAbstractListModel
+class MockListModel : public IQssdVariablesModel
 {
 public:
     MOCK_METHOD(int, rowCount, (const QModelIndex& parent), (const override));
@@ -13,4 +16,9 @@ public:
     MOCK_METHOD(bool, setData,
         (const QModelIndex& index, const QVariant& value, int role),
         (override));
+
+    MOCK_METHOD(
+        QString, getVarValue, (const QString& varName), (const override));
 };
+
+#endif
