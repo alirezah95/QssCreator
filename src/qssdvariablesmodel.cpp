acffcc5f40791af2a6b2f2a1edf2611888f90597
alirezah95 <alirezahassani95@gmail.com>
2022-07-08 12:59:29 +0430

    feat: implement rowCount() and data()

--- src/qssdvariablesmodel.cpp
+++ src/qssdvariablesmodel.cpp
@@ -1,6 +1,37 @@
 #include "qssdvariablesmodel.h"
 
 QssdVariablesModel::QssdVariablesModel(QObject* parent)
-    : QAbstractListModel(parent)
+    : IQssdVariablesModel(parent)
 {
+}
+
+QssdVariablesModel::~QssdVariablesModel() { }
+
+int QssdVariablesModel::rowCount(const QModelIndex& parent) const
+{
+    if (parent.isValid()) {
+        return 0;
+    }
+    return mVariables.size();
+}
+
+QVariant QssdVariablesModel::data(const QModelIndex& index, int role) const
+{
+    if (!index.isValid() || index.row() >= rowCount(QModelIndex())) {
+        return QVariant();
+    }
+
+    QMap<QString, QString>::const_key_value_iterator itm
+        = mVariables.constKeyValueBegin();
+    for (int i = 0; i < index.row(); ++i) {
+        itm++;
+    }
+    switch (role) {
+    case Roles::VariableName:
+        return (*itm).first;
+    case Roles::VariableValue:
+        return (*itm).second;
+    default:
+        return QVariant();
+    }
 }
