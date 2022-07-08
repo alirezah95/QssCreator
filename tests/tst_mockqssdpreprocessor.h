63f58e1aa73880e49ce9e13133c732a223668279
alirezah95 <alirezahassani95@gmail.com>
2022-07-08 11:28:41 +0430

    tests: add a mock for IQssdPreprocessor, add it to mainwinow tests
    Update cmake

---
+++ tests/tst_mockqssdpreprocessor.h
@@ -0,0 +1,22 @@
+#ifndef TST_MOCKQSSDPREPROCESSOR_H
+#define TST_MOCKQSSDPREPROCESSOR_H
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "iqssdpreprocessor.h"
+
+using namespace ::testing;
+
+class MockQssdPreprocessor : public IQssdPreprocessor
+{
+public:
+    MOCK_METHOD(
+        void, setVariablesModel, (IQssdVariablesModel * varsModel), (override));
+    MOCK_METHOD(
+        QString, getProcessedDocumentContent, (QTextEdit * editor), (override));
+    MOCK_METHOD(
+        void, processDocumentVariables, (QTextEdit * editor), (override));
+};
+
+#endif
