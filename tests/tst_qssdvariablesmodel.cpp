ebeffaa780df494e9504114dd6b5179583f34a5e
alirezah95 <alirezahassani95@gmail.com>
2022-07-10 12:42:42 +0430

    tests: add a test case for setData()

--- tests/tst_qssdvariablesmodel.cpp
+++ tests/tst_qssdvariablesmodel.cpp
@@ -35,6 +35,49 @@ TEST_F(TestQssdVariablesModel, TestInsertRows)
     EXPECT_EQ(model->size(), 4);
 }
 
+TEST_F(TestQssdVariablesModel, TestSetData)
+{
+    model->insertRows(0, 4, QModelIndex());
+
+    QSignalSpy dtChangedSpy(model, &QssdVariablesModel::dataChanged);
+
+    model->setData(model->index(0), "var_1", QssdVariablesModel::VariableName);
+    model->setData(model->index(0), "3em", QssdVariablesModel::VariableValue);
+
+    model->setData(model->index(1), "var_2", QssdVariablesModel::VariableName);
+    model->setData(model->index(1), "12px", QssdVariablesModel::VariableValue);
+
+    model->setData(model->index(2), "fgClr", QssdVariablesModel::VariableName);
+    model->setData(model->index(2), "blue", QssdVariablesModel::VariableValue);
+
+    model->setData(
+        model->index(3), "bgColor", QssdVariablesModel::VariableName);
+    model->setData(
+        model->index(3), "#ffff00", QssdVariablesModel::VariableValue);
+
+    EXPECT_EQ(dtChangedSpy.count(), 8);
+    EXPECT_STREQ(model->data(model->index(0), QssdVariablesModel::VariableValue)
+                     .toString()
+                     .toStdString()
+                     .c_str(),
+        "3em");
+    EXPECT_STREQ(model->data(model->index(1), QssdVariablesModel::VariableValue)
+                     .toString()
+                     .toStdString()
+                     .c_str(),
+        "12px");
+    EXPECT_STREQ(model->data(model->index(2), QssdVariablesModel::VariableValue)
+                     .toString()
+                     .toStdString()
+                     .c_str(),
+        "blue");
+    EXPECT_STREQ(model->data(model->index(3), QssdVariablesModel::VariableValue)
+                     .toString()
+                     .toStdString()
+                     .c_str(),
+        "#ffff00");
+}
+
 int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
