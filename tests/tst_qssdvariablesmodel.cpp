09469135a64bc41d00968ee1aba7c818fcfc03a1
alirezah95 <alirezahassani95@gmail.com>
2022-07-10 12:53:10 +0430

    tests: set TestSetData test case to test signal emission in setData()

--- tests/tst_qssdvariablesmodel.cpp
+++ tests/tst_qssdvariablesmodel.cpp
@@ -35,7 +35,7 @@ TEST_F(TestQssdVariablesModel, TestInsertRows)
     EXPECT_EQ(model->size(), 4);
 }
 
-TEST_F(TestQssdVariablesModel, TestSetData)
+TEST_F(TestQssdVariablesModel, TestSetDataSignals)
 {
     model->insertRows(0, 4, QModelIndex());
 
@@ -44,38 +44,20 @@ TEST_F(TestQssdVariablesModel, TestSetData)
     model->setData(model->index(0), "var_1", QssdVariablesModel::VariableName);
     model->setData(model->index(0), "3em", QssdVariablesModel::VariableValue);
 
-    model->setData(model->index(1), "var_2", QssdVariablesModel::VariableName);
-    model->setData(model->index(1), "12px", QssdVariablesModel::VariableValue);
+    EXPECT_EQ(dtChangedSpy.count(), 2);
 
-    model->setData(model->index(2), "fgClr", QssdVariablesModel::VariableName);
-    model->setData(model->index(2), "blue", QssdVariablesModel::VariableValue);
+    auto fstEmission = dtChangedSpy[0];
+    auto scndEmission = dtChangedSpy[1];
 
-    model->setData(
-        model->index(3), "bgColor", QssdVariablesModel::VariableName);
-    model->setData(
-        model->index(3), "#ffff00", QssdVariablesModel::VariableValue);
+    EXPECT_EQ(fstEmission.at(0).toModelIndex().row(), 0);
+    EXPECT_EQ(fstEmission.at(1).toModelIndex().row(), 0);
+    EXPECT_TRUE(fstEmission.at(2).toList().at(0).toInt()
+        == QssdVariablesModel::VariableName);
 
-    EXPECT_EQ(dtChangedSpy.count(), 8);
-    EXPECT_STREQ(model->data(model->index(0), QssdVariablesModel::VariableValue)
-                     .toString()
-                     .toStdString()
-                     .c_str(),
-        "3em");
-    EXPECT_STREQ(model->data(model->index(1), QssdVariablesModel::VariableValue)
-                     .toString()
-                     .toStdString()
-                     .c_str(),
-        "12px");
-    EXPECT_STREQ(model->data(model->index(2), QssdVariablesModel::VariableValue)
-                     .toString()
-                     .toStdString()
-                     .c_str(),
-        "blue");
-    EXPECT_STREQ(model->data(model->index(3), QssdVariablesModel::VariableValue)
-                     .toString()
-                     .toStdString()
-                     .c_str(),
-        "#ffff00");
+    EXPECT_EQ(scndEmission.at(0).toModelIndex().row(), 0);
+    EXPECT_EQ(scndEmission.at(1).toModelIndex().row(), 0);
+    EXPECT_TRUE(scndEmission.at(2).toList().at(0).toInt()
+        == QssdVariablesModel::VariableValue);
 }
 
 int main(int argc, char* argv[])
