50c78f6f68673fc792e245f26b53fbd66b9a003f
alirezah95 <alirezahassani95@gmail.com>
2022-07-17 16:25:48 +0430

    feat: set item delegate to draw rect if value is color
    And draw the value inside that rect

--- src/qssdvariableitemdelegate.cpp
+++ src/qssdvariableitemdelegate.cpp
@@ -21,6 +21,7 @@ void QssdVariableItemDelegate::paint(QPainter* painter,
 {
     if (index.isValid()) {
         painter->save();
+
         painter->fillRect(option.rect, option.palette.base());
 
         auto vName
@@ -38,7 +39,30 @@ void QssdVariableItemDelegate::paint(QPainter* painter,
         // Draw var value
         QRectF vValueRect(adjustedRect.topLeft() + QPoint(nameWidth, 0),
             QSizeF(adjustedRect.width() - nameWidth, adjustedRect.height()));
-        painter->drawText(vValueRect, Qt::AlignLeft | Qt::AlignVCenter, vValue);
+        if (QColor::isValidColor(vValue)) {
+            painter->save();
+
+            // Draw a rectangle with the color specified by the var value
+            QRectF colorRect(vValueRect.topLeft() + QPoint(1, 1),
+                QSize(option.fontMetrics.boundingRect(vValue).width() + 4,
+                    vValueRect.height() - 4));
+            // Draw a rect border with text color
+            QColor bgColor(vValue);
+            painter->drawRect(colorRect);
+            painter->fillRect(colorRect.adjusted(1, 1, 0, 0), QBrush(bgColor));
+
+            // Draw the value text inside the colorRect
+            QColor valueTxtColor;
+            valueTxtColor.setRgb(
+                bgColor.lightnessF() < 0.5 ? qRgb(1, 1, 1) : qRgb(0, 0, 0));
+            painter->setPen(QPen(valueTxtColor));
+            painter->drawText(colorRect, Qt::AlignVCenter, vValue);
+
+            painter->restore();
+        } else {
+            painter->drawText(
+                vValueRect, Qt::AlignLeft | Qt::AlignVCenter, vValue);
+        }
 
         // Draw the border it view item is selected
         if (option.state & QStyle::State_Selected) {
