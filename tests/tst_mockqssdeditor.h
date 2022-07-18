75c66c3c8604380127eccefd070e825366fc8a7e
alirezah95 <alirezahassani95@gmail.com>
2022-07-18 13:29:06 +0430

    tests: update qssdeditor mock with changes in IQssdEditor

--- tests/tst_mockqssdeditor.h
+++ tests/tst_mockqssdeditor.h
@@ -13,6 +13,7 @@ public:
 
     MOCK_METHOD(void, setProcessor, (IQssdProcessor*), (override));
     MOCK_METHOD(IQssdProcessor*, getProcessor, (), (const override));
+    MOCK_METHOD(QString, getQtStylesheet, (bool update), (override));
     MOCK_METHOD(int, getLineNumbersAreaWidth, (), (const override));
     MOCK_METHOD(void, setLineNumbersFont, (QFont font), (override));
     MOCK_METHOD(QFont, getLineNumbersFont, (), (const override));
