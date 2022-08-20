e8125a128b7ee08f93b44b653818ab046452ae45
alirezah95 <alirezahassani95@gmail.com>
2022-08-20 16:56:51 +0430

    build: change Windows installer name

--- win-package.cmake
+++ win-package.cmake
@@ -1,7 +1,11 @@
+set(INSTALLER_NAME
+    "${PROJECT_NAME}-${PROJECT_VERSION}"
+)
+
 install(
     TARGETS ${PROJECT_NAME}
     RUNTIME DESTINATION .
-    COMPONENT "${PROJECT_NAME}Installer"
+    COMPONENT "${INSTALLER_NAME}"
 )
 
 include(InstallRequiredSystemLibraries)
@@ -20,13 +24,13 @@ if (EXISTS "$ENV{QTDIR}/bin/windeployqt.exe")
             install(
                 DIRECTORY ${item}
                 DESTINATION .
-                COMPONENT "${PROJECT_NAME}Installer"
+                COMPONENT "${INSTALLER_NAME}"
             )
         else()
             install(
                 FILES ${item}
                 DESTINATION .
-                COMPONENT "${PROJECT_NAME}Installer"
+                COMPONENT "${INSTALLER_NAME}"
             )
         endif()
     endforeach()
@@ -34,12 +38,12 @@ if (EXISTS "$ENV{QTDIR}/bin/windeployqt.exe")
     set(CPACK_GENERATOR IFW)
 
     set(CPACK_PACKAGE_NAME ${PROJECT_NAME})
-    set(CPACK_PACKAGE_FILE_NAME "${PROJECT_NAME}Installer")
+    set(CPACK_PACKAGE_FILE_NAME "${INSTALLER_NAME}")
     set(CPACK_PACKAGE_VENDOR "Alireza Hs")
     set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Install Qss Creator Application")
     set(CPACK_PACKAGE_VERSION ${CMAKE_PROJECT_VERSION})
     SET(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_SOURCE_DIR}/LICENSE")
-    set(CPACK_COMPONENTS_ALL "${PROJECT_NAME}Installer")
+    set(CPACK_COMPONENTS_ALL "${INSTALLER_NAME}")
     set(CMAKE_PROJECT_HOMEPAGE_URL "https://github.com/alirezah95/QssCreator")
 
     set(CPACK_IFW_PACKAGE_START_MENU_DIRECTORY Qt)
@@ -51,13 +55,13 @@ if (EXISTS "$ENV{QTDIR}/bin/windeployqt.exe")
     include(CPack REQUIRED)
     include(CPackIFW REQUIRED)
 
-    cpack_add_component("${PROJECT_NAME}Installer"
+    cpack_add_component("${INSTALLER_NAME}"
         DISPALY_NAME "Qss Creator Installer"
         DESCRIPTION "Install Qss Creator"
         REQUIRED
     )
 
-    cpack_ifw_configure_component("${PROJECT_NAME}Installer"
+    cpack_ifw_configure_component("${INSTALLER_NAME}"
         ESSENTIAL
         DEFAULT_TRUE
         NAME "oss.alirezah.qsscreator"
@@ -67,7 +71,7 @@ if (EXISTS "$ENV{QTDIR}/bin/windeployqt.exe")
     )
     install(FILES ${CMAKE_SOURCE_DIR}/LICENSE
         DESTINATION "."
-        COMPONENT "${PROJECT_NAME}Installer"
+        COMPONENT "${INSTALLER_NAME}"
     )
 else()
     message("Unable to find QTDIR/bin/windeployqt.exe")
